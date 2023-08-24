084b3c55db1e19d37a1e59acc90078f637f7c8d0
shallow90125 <shallow90125@gmail.com>
2023-08-24 23:07:27 +0900

    fix types

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,7 +2,7 @@ import { DiscordEvent } from "@/interfaces";
 
 export const ready: DiscordEvent<"ready"> = {
   name: "ready",
-  process: (client) => {
+  process: async (client) => {
     console.log(`ready: ${client.user.id}`);
   },
 };
